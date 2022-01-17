1195a51e5e07d7d2c33a9b7e76fe537da401aa47
flash_hammad <flash_hammad@yahoo.co.in>
2022-01-17 14:16:33 +0530

    an updated commit message

---
+++ primef.py
@@ -0,0 +1,32 @@
+import math
+
+# A function to print all prime factors of 
+# a given number n
+def primeFactors(n):
+    
+    # Print the number of two's that divide n
+    #this applies a cnocept that smaller if went through wont come again
+    while n % 2 == 0:
+        print(2),
+        n = n / 2
+        
+    # n must be odd at this point
+    # so a skip of 2 ( i = i + 2) can be used
+    for i in range(3,int(math.sqrt(n))+1,2):
+        
+        # while i divides n , print i and divide n
+        print("sqrt(n))-->itrstion", math.sqrt(n), i)
+        while n % i== 0:
+            print(i),
+            n = n // i
+            
+    # Condition if n is a prime
+    # number greater than 2
+    if n > 2:
+        print(round(n))
+
+        # Driver Program to test above function
+
+n = int(input("enter the number for which we need prime factor"))
+primeFactors(n)
+        
